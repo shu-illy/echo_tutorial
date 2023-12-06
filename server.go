7518e2035973d2e43ebc279e03d0206cebaf6f00
shu-illy <sh.illy.9117@gmail.com>
2023-12-07 01:59:16 +0900

    Quick Start

---
+++ server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!!")
+	})
+	e.POST("/users", saveUser)
+	e.POST("/save", save)
+	e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+	e.Logger.Fatal(e.Start("localhost:1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func saveUser(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name: "+name+", email: "+email)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
+}
